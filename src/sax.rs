70c792bafb3dafdcc96a55c230513c281a33d82f
Brandon Sanderson <bas.bsanders@gmail.com>
2014-07-09 20:37:13 -0700

    Fix tests, didn't run them before last fix.

--- src/sax.rs
+++ src/sax.rs
@@ -210,8 +210,8 @@ mod tests {
             match sax.recv() {
                 Ok(StartDocument) => (),
                 Ok(EndDocument) => break,
-                Ok(event) => println!("{}", event.to_str()),
-                Err(err) => println!("{}", err.to_str()),
+                Ok(event) => println!("{}", event.to_string()),
+                Err(err) => println!("{}", err.to_string()),
             }
         }
     }
